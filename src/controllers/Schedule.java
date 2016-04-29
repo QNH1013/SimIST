c7ccf72c2156857bb149d2b79f4a33902ec321f9
QNH1013 <QNH1013@users.noreply.github.com>
2016-04-29 19:56:29 -0400

    made to work with buttons

--- src/controllers/Schedule.java
+++ src/controllers/Schedule.java
@@ -1,40 +1,43 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package controllers;
-//import org.jsoup.nodes.*;
-//import org.jsoup.parser.*;
-//import org.jsoup.select.Elements;
-//import java.io.*;
-//import javax.lang.model.util.Elements;
-//import javax.swing.text.Document;
-//import org.jsoup.Jsoup;
-///**
-// *
-// * @author Colin
-// */
-//public class Schedule {
-//    
-//    public Schedule(){
-//        
-//    }
-//    
-//    public void parseScheduleDocument(){
-//        File inputSchedule = new File("src/controllers/ISTschedule.htm");
-//        try {
-//            Document html = Jsoup.parse(inputSchedule, "UTF-8");
-//            Elements allCourses = html.body().getElementsByClass("course_abbrev");
-//            String test = allCourses.text();
-//            System.out.print(test);
-//            
-//            Elements moreInfo = html.body().getElementsByClass("course_description");
-//            String test2 = moreInfo.text();
-//            System.out.printf(test2);
-//        }catch (IOException e){
-//            System.out.printf("Fail");
-//        }
-//        
-//    }
-//}
+package controllers;
+import org.jsoup.nodes.*;
+import org.jsoup.parser.*;
+import org.jsoup.select.Elements;
+import java.io.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
+
+public class Schedule extends JPanel  {
+    
+    private JPanel sch;
+    private JLabel classInfo;
+    private JTextArea course_abbrev;
+    private JTextArea class_details;
+    
+    
+    
+    public Schedule(){
+        setLayout(null);
+        JFrame frame = new JFrame();
+        frame.setSize(600,600);
+        classInfo = new JLabel("IST 2015-2016 Schedule");
+        classInfo.setEnabled(false);
+        sch = new JPanel();
+       
+            File inputSchedule = new File("src/controllers/ISTschedule.htm");
+         try {
+            Document html = Jsoup.parse(inputSchedule, "UTF-8");
+            Elements allCourses = html.body().getElementsByClass("course_abbrev");
+            String test = allCourses.text();
+            course_abbrev = new JTextArea(test);
+            Elements moreInfo = html.body().getElementsByClass("course_description");
+            String test2 = moreInfo.text();
+            class_details = new JTextArea(test2);
+        }catch (IOException e){
+            System.out.printf("Fail");
+        }
+        
+    }
+}
